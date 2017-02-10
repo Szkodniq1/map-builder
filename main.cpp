ad980400bf12023cef1588242c9d4c3929faa63b
Piotr Gomoła <szkodniq@gmail.com>
2017-02-10 16:59:58 +0100

    Add grabber functions, and octomap includes

--- main.cpp
+++ main.cpp
@@ -1,46 +1,163 @@
-#include <iostream>
-#include "3rdParty/tinyXML/tinyxml2.h"
-#include <opencv2/opencv.hpp>
-#include <pcl/point_cloud.h>
+#include <pcl/io/openni_grabber.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/console/parse.h>
 
-using namespace cv;
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+
+#include <iostream>
+
 using namespace std;
+using namespace pcl;
+using namespace octomap;
 
+PointCloud<PointXYZRGBA>::Ptr cloudptr(new PointCloud<PointXYZRGBA>); // A cloud that will store color info.
+PointCloud<PointXYZ>::Ptr fallbackCloud(new PointCloud<PointXYZ>);    // A fallback cloud with just depth data.
+boost::shared_ptr<visualization::CloudViewer> viewer;                 // Point cloud viewer object.
+Grabber* openniGrabber;                                               // OpenNI grabber that takes data from the device.
+unsigned int filesSaved = 0;                                          // For the numbering of the clouds saved to disk.
+bool saveCloud(false), noColor(false);                                // Program control.
+OcTree tree (0.1);
 
-int main()
+void
+printUsage(const char* programName)
 {
-    tinyxml2::XMLDocument config;
-    config.LoadFile("../map-builder/resources/config.xml");
-    if (config.ErrorID())
-        cout << "unable to load config file.\n";
-    string imagePath(config.FirstChildElement( "TestSource" )->FirstChildElement( "path" )->GetText());
+    cout << "Usage: " << programName << " [options]"
+         << endl
+         << endl
+         << "Options:\n"
+         << endl
+         << "\t<none>     start capturing from an OpenNI device.\n"
+         << "\t-v FILE    visualize the given .pcd file.\n"
+         << "\t-h         shows this help.\n";
+}
 
-    Mat rgb, depth;
-    rgb = imread( imagePath + "/rgb/0.png", 1 );
-    depth = imread(imagePath + "/depth/0.png", 1);
-    cvtColor( depth, depth, CV_BGR2GRAY );
+// This function is called every time the device has new data.
+void
+grabberCallback(const PointCloud<PointXYZRGBA>::ConstPtr& cloud)
+{
+    if (! viewer->wasStopped())
+        viewer->showCloud(cloud);
 
-    namedWindow("rgb", 1);
-    imshow("rgb", rgb);
-    namedWindow("depth", 1);
-    imshow("depth", depth);
-
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::PCLPointCloud2 cloud_blob;
-    pcl::io::loadPCDFile (imagePath + "/pcd/0.pcd", cloud_blob);
-    pcl::fromPCLPointCloud2 (cloud_blob, *cloud);
-
-    pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-    viewer.showCloud(cloud);
-
-    while(1) {
-        if(waitKey(30) == 0) {
-            break;
+    if (saveCloud)
+    {
+        stringstream stream;
+        stream << "inputCloud" << filesSaved << ".pcd";
+        string filename = stream.str();
+        if (io::savePCDFile(filename, *cloud, true) == 0)
+        {
+            filesSaved++;
+            cout << "Saved " << filename << "." << endl;
         }
+        else PCL_ERROR("Problem saving %s.\n", filename.c_str());
+
+        saveCloud = false;
+    }
+}
+
+// For detecting when SPACE is pressed.
+void
+keyboardEventOccurred(const visualization::KeyboardEvent& event,
+                      void* nothing)
+{
+    if (event.getKeySym() == "space" && event.keyDown())
+        saveCloud = true;
+}
+
+// Creates, initializes and returns a new viewer.
+boost::shared_ptr<visualization::CloudViewer>
+createViewer()
+{
+    boost::shared_ptr<visualization::CloudViewer> v
+    (new visualization::CloudViewer("OpenNI viewer"));
+    v->registerKeyboardCallback(keyboardEventOccurred);
+
+    return (v);
+}
+
+int
+main(int argc, char** argv)
+{
+
+    if (console::find_argument(argc, argv, "-h") >= 0)
+    {
+        printUsage(argv[0]);
+        return -1;
     }
 
-    return 0;
+    bool justVisualize(false);
+    string filename;
+    if (console::find_argument(argc, argv, "-v") >= 0)
+    {
+        if (argc != 3)
+        {
+            printUsage(argv[0]);
+            return -1;
+        }
+
+        filename = argv[2];
+        justVisualize = true;
+    }
+    else if (argc != 1)
+    {
+        printUsage(argv[0]);
+        return -1;
+    }
+
+    // First mode, open and show a cloud from disk.
+    if (justVisualize)
+    {
+        // Try with color information...
+        try
+        {
+            io::loadPCDFile<PointXYZRGBA>(filename.c_str(), *cloudptr);
+        }
+        catch (PCLException e1)
+        {
+            try
+            {
+                // ...and if it fails, fall back to just depth.
+                io::loadPCDFile<PointXYZ>(filename.c_str(), *fallbackCloud);
+            }
+            catch (PCLException e2)
+            {
+                return -1;
+            }
+
+            noColor = true;
+        }
+
+        cout << "Loaded " << filename << "." << endl;
+        if (noColor)
+            cout << "This cloud has no RGBA color information present." << endl;
+        else cout << "This cloud has RGBA color information present." << endl;
+    }
+    // Second mode, start fetching and displaying frames from the device.
+    else
+    {
+        openniGrabber = new OpenNIGrabber();
+        if (openniGrabber == 0)
+            return -1;
+        boost::function<void (const PointCloud<PointXYZRGBA>::ConstPtr&)> f =
+            boost::bind(&grabberCallback, _1);
+        openniGrabber->registerCallback(f);
+    }
+
+    viewer = createViewer();
+
+    if (justVisualize)
+    {
+        if (noColor)
+            viewer->showCloud(fallbackCloud);
+        else viewer->showCloud(cloudptr);
+    }
+    else openniGrabber->start();
+
+    // Main loop.
+    while (! viewer->wasStopped())
+        boost::this_thread::sleep(boost::posix_time::seconds(1));
+
+    if (! justVisualize)
+        openniGrabber->stop();
 }
