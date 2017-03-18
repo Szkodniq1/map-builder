18e07cc4918f73b24bfb7006e72529165dade2e9
Piotr Gomoła <szkodniq@gmail.com>
2017-03-18 16:13:27 +0100

    Best solution chosen according to performance tests, current problem - inserted point cloud is mirrored

--- src/Map/octomap.cpp
+++ src/Map/octomap.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <opencv2/core.hpp>
 
 /// A single instance of Octomap
 OctoMap::Ptr octoMap;
@@ -32,7 +33,6 @@ void OctoMap::insertCloud(mapping::PointCloud& PC, octomap::pose6d pose){
     Eigen::Translation<float, 3> translation (pose.trans().x(), pose.trans().y(), pose.trans().z());
     // Assemble an Eigen Transform
     Eigen::Quaternion<float> pos(pose.rot().u(),pose.rot().x(), pose.rot().y(), pose.rot().z());
-    //Eigen::Quaternion<float> pos(1,0, 0, 0);
     Eigen::Transform<float, 3, Eigen::Affine> transform (translation * pos);
 
 
@@ -41,37 +41,52 @@ void OctoMap::insertCloud(mapping::PointCloud& PC, octomap::pose6d pose){
     std::cout<<"Vector before normalisation :"<<pos.norm()<<std::endl;
     pos.normalize();
     std::cout<<"Vector normalised :"<<pos.norm()<<std::endl;
+    int64 e1 = cv::getTickCount();
     for(mapping::Point3D point : PC) {
-/*        Eigen::Vector3f pt (point.position.x(), point.position.y(), point.position.z());
-        Eigen::Quaternion<float> p;
-        p.w() = 0;
-        p.vec() = pt;
-        Eigen::Quaternion<float> rotatedP = pos * p * pos.inverse();
-        Eigen::Vector3f rotatedPt = rotatedP.vec();
-        rotatedPt[0] += translation.x();
-        rotatedPt[1] += translation.y();
-        rotatedPt[2] += translation.z();
+        /// Performance +/- 0,6s left as it more readable solution
+        Eigen::Matrix<float, 3, 1> pt (point.position.x(), point.position.y(), point.position.z());
+                newPC.push_back(mapping::Point3D(
+                            static_cast<float> (transform (0, 0) * pt.coeffRef (0) + transform (0, 1) * pt.coeffRef (1) + transform (0, 2) * pt.coeffRef (2) + transform (0, 3)),
+                            static_cast<float> (transform (1, 0) * pt.coeffRef (0) + transform (1, 1) * pt.coeffRef (1) + transform (1, 2) * pt.coeffRef (2) + transform (1, 3)),
+                            static_cast<float> (transform (2, 0) * pt.coeffRef (0) + transform (2, 1) * pt.coeffRef (1) + transform (2, 2) * pt.coeffRef (2) + transform (2, 3)),point.color.r,
+                            point.color.g,
+                            point.color.b,
+                            point.color.a));
 
-        newPC.push_back(mapping::Point3D(
-                            rotatedPt[0],
-                            rotatedPt[1],
-                            rotatedPt[2],
-                            point.color.r,
-                            point.color.g,
-                            point.color.b,
-                            point.color.a));
-                            */
-        Eigen::Vector3f pt (point.position.x(), point.position.y(), point.position.z());
-        newPC.push_back(mapping::Point3D(
-                            (1-2*pos.y()*pos.y()-2*pos.z()*pos.z())*point.position.x() + 2*(pos.x()*pos.y() - pos.w()*pos.z())*point.position.y() + 2*(pos.x()*pos.z() + pos.w()*pos.y())*point.position.z() + translation.x(),
-                            2*(pos.x()*pos.y() + pos.w()*pos.z())*point.position.x() + (1-2*pos.x()*pos.x()-2*pos.z()*pos.z())*point.position.y() + 2*(pos.y()*pos.z() + pos.w()*pos.x())*point.position.z() + translation.y(),
-                            2*(pos.x()*pos.z() - pos.w()*pos.y())*point.position.x() + 2*(pos.y()*pos.z() - pos.w()*pos.x())*point.position.y() + (1-2*pos.x()*pos.x()-2*pos.y()*pos.y())*point.position.z() + translation.z(),
-                            point.color.r,
-                            point.color.g,
-                            point.color.b,
-                            point.color.a));
+        /// Performance +/- 1,5s
+//        Eigen::Vector3f pt (point.position.x(), point.position.y(), point.position.z());
+//        Eigen::Quaternion<float> p;
+//        p.w() = 0;
+//        p.vec() = pt;
+//        Eigen::Quaternion<float> rotatedP = pos * p * pos.inverse();
+//        Eigen::Vector3f rotatedPt = rotatedP.vec();
+//        rotatedPt[0] += translation.x();
+//        rotatedPt[1] += translation.y();
+//        rotatedPt[2] += translation.z();
+
+//        newPC.push_back(mapping::Point3D(
+//                            rotatedPt[0],
+//                            rotatedPt[1],
+//                            rotatedPt[2],
+//                            point.color.r,
+//                            point.color.g,
+//                            point.color.b,
+//                            point.color.a));
+
+        /// Performance +/- 0,6s
+//        Eigen::Vector3f pt (point.position.x(), point.position.y(), point.position.z());
+//        newPC.push_back(mapping::Point3D(
+//                            (1-2*pos.y()*pos.y()-2*pos.z()*pos.z())*point.position.x() + 2*(pos.x()*pos.y() - pos.w()*pos.z())*point.position.y() + 2*(pos.x()*pos.z() + pos.w()*pos.y())*point.position.z() + translation.x(),
+//                            2*(pos.x()*pos.y() + pos.w()*pos.z())*point.position.x() + (1-2*pos.x()*pos.x()-2*pos.z()*pos.z())*point.position.y() + 2*(pos.y()*pos.z() + pos.w()*pos.x())*point.position.z() + translation.y(),
+//                            2*(pos.x()*pos.z() - pos.w()*pos.y())*point.position.x() + 2*(pos.y()*pos.z() - pos.w()*pos.x())*point.position.y() + (1-2*pos.x()*pos.x()-2*pos.y()*pos.y())*point.position.z() + translation.z(),
+//                            point.color.r,
+//                            point.color.g,
+//                            point.color.b,
+//                            point.color.a));
     }
-
+    int64 e2 = cv::getTickCount();
+    double time = ((e2 - e1)/ cv::getTickFrequency());
+    std::cout<<"Calc time: "<<time<<std::endl;
     octoCloud.reserve(newPC.size());
     for(mapping::Point3D point : newPC) {
         octoCloud.push_back(
@@ -79,8 +94,8 @@ void OctoMap::insertCloud(mapping::PointCloud& PC, octomap::pose6d pose){
                     point.position.y(),
                     point.position.z());
     }
-    //this->map.insertPointCloud(octoCloud, pose.trans());
-    //this->map.updateInnerOccupancy();
+    this->map.insertPointCloud(octoCloud, pose.trans());
+    this->map.updateInnerOccupancy();
 
     notify(newPC);
 }
