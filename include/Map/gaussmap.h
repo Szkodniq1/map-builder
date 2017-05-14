ac0bd5a7c111086ed61ba600c8443648373c2f16
Wojciech Maćkowiak <Skorpius93@wp.pl>
2017-05-14 12:48:11 +0200

    Added functions required to update voxels

--- include/Map/gaussmap.h
+++ include/Map/gaussmap.h
@@ -5,6 +5,7 @@
 #include "3rdParty/octree/octree.h"
 #include "map.h"
 
+#define MAP_SIZE 256
 
 namespace mapping {
     /// create a single Maping object
@@ -18,12 +19,27 @@ private:
     const double res = 0.1;
     PointCloud cloud;
 
+    //Map borders
+    double xmin, xmax;
+    double ymin, ymax;
+    double zmin, zmax;
+
+
+protected:
+    //Protected functions
+    void updateMap();
+    void updateVoxel(int xCoor, int yCoor, int zCoor, mapping::Point3D point);
+    int xCoordinate(double x);
+    int yCoordinate(double y);
+    int zCoordinate(double z);
+
 public:
     /// Pointer
     typedef std::unique_ptr<Gaussmap> Ptr;
 
     Gaussmap(void);
     Gaussmap(mapping::PointCloud PC);
+    Gaussmap(mapping::PointCloud PC, float vxmin, float vxmax, float vymin, float vymax, float vzmin, float vzmax);
 
     /// Name of the map
     const std::string& getName() const {return name;}
--- src/Map/gaussmap.cpp
+++ src/Map/gaussmap.cpp
@@ -3,17 +3,29 @@
 /// A single instance of Octomap
 Gaussmap::Ptr gaussMap;
 
-Gaussmap::Gaussmap(void) : map(256) {
-
+Gaussmap::Gaussmap(void) : map(MAP_SIZE) {
+    xmin = ymin = zmin = -1 * MAP_SIZE * res/2;
+    xmax = ymax = zmax = -1* xmin;
 }
 
-Gaussmap::Gaussmap(mapping::PointCloud PC) : map(256) {
+Gaussmap::Gaussmap(mapping::PointCloud PC) : map(MAP_SIZE) {
+    cloud = PC;
+    xmin = ymin = zmin = -1 * MAP_SIZE * res/2;
+    xmax = ymax = zmax = -1* xmin;
+}
 
+Gaussmap::Gaussmap(mapping::PointCloud PC, float vxmin, float vxmax, float vymin, float vymax, float vzmin, float vzmax) : map(MAP_SIZE) {
+    cloud = PC;
+    xmin = vxmin; xmax = vxmax;
+    ymin = vymin; ymax = vymax;
+    zmin = vzmin; zmax = vzmax;
 }
 
 /// Insert point cloud into map
 void Gaussmap::insertCloud(mapping::GrabbedImage grab, bool isLast) {
+    cloud = grab.transformedPointCloud();
 
+    updateMap();
 }
 
 /// save map in file
@@ -40,3 +52,67 @@ mapping::Map* mapping::createMapGauss(PointCloud PC) {
     gaussMap.reset(new Gaussmap(PC));
     return gaussMap.get();
 }
+
+
+void Gaussmap::updateMap() {
+    int xCoor, yCoor, zCoor;
+    for(mapping::Point3D &point : cloud) {
+        xCoor = xCoordinate(point.position.x());
+        yCoor = yCoordinate(point.position.y());
+        zCoor = zCoordinate(point.position.z());
+
+        updateVoxel(xCoor, yCoor, zCoor, point);
+    }
+}
+
+void Gaussmap::updateVoxel(int xCoor, int yCoor, int zCoor, mapping::Point3D point) {
+    Voxel *tmp = &map(xCoor, yCoor, zCoor);
+
+    if((tmp->mean.x() == 0) && (tmp->mean.y() == 0) && (tmp->mean.z() == 0)) {
+        tmp->mean = Vec3(point.position.x(), point.position.y(), point.position.z());
+        tmp->dev(0,0) = tmp->dev(1,1) = tmp->dev(2,2) = res/2;
+        return;
+    }
+
+
+}
+
+int Gaussmap::xCoordinate(double x) {
+    double  a = MAP_SIZE/(xmax-xmin);
+    double b = -1 * a*xmin;
+    return a*x + b;
+}
+
+int Gaussmap::yCoordinate(double y) {
+    double  a = MAP_SIZE/(ymax-ymin);
+    double b = -1 * a*ymin;
+    return a*y + b;
+}
+
+int Gaussmap::zCoordinate(double z) {
+    double  a = MAP_SIZE/(zmax-zmin);
+    double b = -1 * a*zmin;
+    return a*z + b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
